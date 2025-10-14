4f184937a98eb8cb1f1b456c7cf3cc10656514f9
yifanfeng97 <evanfeng97@gmail.com>
2025-10-14 16:36:53 +0800

    chore: update version to 0.4.0-dev and add unreleased section in changelog

--- hyperdb/__init__.py
+++ hyperdb/__init__.py
@@ -2,6 +2,6 @@ from ._global import AUTHOR_EMAIL  # noqa: F401
 from .base import BaseHypergraphDB  # noqa: F401
 from .hypergraph import HypergraphDB  # noqa: F401
 
-__version__ = "0.3.0"
+__version__ = "0.4.0-dev"
 
 __all__ = ["AUTHOR_EMAIL", "BaseHypergraphDB", "HypergraphDB"]
